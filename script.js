1b89dbc89b15bb04f1543f32b9ab242047e0ef53
Raiyan Razi <raiyanrazi@Raiyans-MacBook-Pro.local>
2021-01-06 11:19:32 +0530

    deploy 2

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
   e.preventDefault();
   if(e.target.classList.contains('nav__link')){
     const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    document.querySelector().scrollIntoView({behavior: 'smooth'});
   }
 })
 
